d7a2d60a95e77df7001474683a69892d1552e505
kumatira <kumano@val.co.jp>
2022-07-07 14:23:31 +0900

    不要な記述、ファイルを削除

--- source/javascripts/sidemenu.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.scrollspy');
-    var options = {
-      scrollOffset: 200
-    };
-    var instances = M.ScrollSpy.init(elems, options);
-  });
